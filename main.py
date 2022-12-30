06eb553a7ed993226cdd27a85f6ad7fb72ec0a7f
Ruki <rukiren2004@gmail.com>
2022-12-31 03:43:50 +0800

    架構調整

--- main.py
+++ main.py
@@ -4,14 +4,15 @@ from system.questionOut import printQuestion
 
 while True:
     question = input('選擇題目:')
-    # try:
-    printQuestion(question=question)
-    # except:
-    #     print("題目不存在")
-    #     break
+    try:
+        printQuestion(question=question)
+    except:
+        print("題目不存在")
+        break
     test = input('選擇檔案路徑:')
     language = input("選擇語言：\n1 = C/C++ \n2 = python\n")
     if language == '1':
-            cppCompiler(question=question, test=test)
+            state = cppCompiler(question=question, test=test)
     elif language == "2" :
-            pythonCompiler(question=question, test=test)
+            state = pythonCompiler(question=question, test=test)
+    print(state[0])
